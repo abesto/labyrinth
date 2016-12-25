9ba3e6eae66f4db4a82db88caf2a321a08015722
Zoltan Nagy <abesto@abesto.net>
2016-12-25 23:44:27 +0100

    Store book content in plain text files, referenced from JSON

--- game/src/main/scala/net/abesto/labyrinth/components/PopupTriggerComponent.scala
+++ game/src/main/scala/net/abesto/labyrinth/components/PopupTriggerComponent.scala
@@ -3,12 +3,5 @@ package net.abesto.labyrinth.components
 import com.artemis.Component
 
 class PopupTriggerComponent extends Component {
-  var title: String = ""
-  var text: String = ""
-
-  def this(title: String, text: String) {
-    this()
-    this.title = title
-    this.text = text
-  }
+  var source: String = ""
 }
--- game/src/main/scala/net/abesto/labyrinth/systems/PopupTriggerSystem.scala
+++ game/src/main/scala/net/abesto/labyrinth/systems/PopupTriggerSystem.scala
@@ -9,6 +9,7 @@ import net.abesto.labyrinth.maze.ShallowWaterTile
 import net.mostlyoriginal.api.event.common.{EventSystem, Subscribe}
 
 import scala.collection.immutable.Queue
+import scala.io.Source
 
 class PopupTriggerSystem extends BaseSystem {
   var eventSystem: EventSystem = _
@@ -29,9 +30,10 @@ class PopupTriggerSystem extends BaseSystem {
       Aspect.all(classOf[PositionComponent], classOf[PopupTriggerComponent])
     ).find(id => positionMapper.get(id).coord == position).foreach(entityId => {
       val trigger = popupTriggerMapper.get(entityId)
-      eventSystem.dispatch(ShowPopupEvent(
-        trigger.title, trigger.text
-      ))
+      val lines = Source.fromURL(getClass.getResource(trigger.source)).getLines()
+      val title = lines.next()
+      val text = lines.mkString("\n")
+      eventSystem.dispatch(ShowPopupEvent(title, text))
     })
   }
 
